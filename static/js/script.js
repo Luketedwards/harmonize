37f1309ef5be4f680766acd20fa74f49ca0f4d24
Luketedwards <Luketedwards96@hotmail.com>
2022-06-07 16:17:07 +0000

    Various console errors fixed

--- static/js/script.js
+++ static/js/script.js
@@ -51,28 +51,5 @@
   });
   
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {
-    direction: 'left'
-  });
-});
-
-$(document).ready(function(){
-var password = document.getElementById("password")
-  , confirm_password = document.getElementById("passwordConfirm");
-
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
-}
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
-});    
-
 
 
