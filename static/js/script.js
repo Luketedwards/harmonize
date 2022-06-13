1e2599143bb72bb5bfd392029e5de5ff3a939a88
Luketedwards <Luketedwards96@hotmail.com>
2022-06-13 13:18:38 +0000

    Tidied up script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,47 +3,47 @@
     $('.sidenav').sidenav();
     $('select').formSelect();
     $('ul.tabs').tabs();
-    $('#file-upload').bind('change', function() { var fileName = ''; fileName = $(this).val(); $('#file-selected').html(fileName).css("visibility", "visible");; })
+    $('#file-upload').bind('change', function() { var fileName = ''; fileName = $(this).val(); $('#file-selected').html(fileName).css("visibility", "visible");});
     $('.collapsible').collapsible();
     $(".dropdown-trigger").dropdown();
     $(".dropdown-trigger-side").dropdown();
     $('input#input_text, textarea#project-description').characterCounter();
     $('.modal').modal();
     if(window.innerWidth < 1000){
-      $('#about-the-project').addClass('center')
-      $('#instruments-and-genre').addClass('center')
-      $('#about-the-project').removeClass('left')
-      $('#instruments-and-genre').removeClass('right')
+      $('#about-the-project').addClass('center');
+      $('#instruments-and-genre').addClass('center');
+      $('#about-the-project').removeClass('left');
+      $('#instruments-and-genre').removeClass('right');
     }else{
-      $('#about-the-project').removeClass('center')
-      $('#instruments-and-genre').removeClass('center')
-      $('#about-the-project').addClass('left')
-      $('#instruments-and-genre').addClass('right')
+      $('#about-the-project').removeClass('center');
+      $('#instruments-and-genre').removeClass('center');
+      $('#about-the-project').addClass('left');
+      $('#instruments-and-genre').addClass('right');
     }
   });
 
   $(window).resize(function(){
     if(window.innerWidth < 1000){
-      $('#about-the-project').addClass('center')
-      $('#instruments-and-genre').addClass('center')
+      $('#about-the-project').addClass('center');
+      $('#instruments-and-genre').addClass('center');
      
     }else{
-      $('#about-the-project').removeClass('center')
-      $('#instruments-and-genre').removeClass('center')
-      $('#about-the-project').addClass('left')
-      $('#instruments-and-genre').addClass('right')
+      $('#about-the-project').removeClass('center');
+      $('#instruments-and-genre').removeClass('center');
+      $('#about-the-project').addClass('left');
+      $('#instruments-and-genre').addClass('right');
     }
     
   });
   $(window).resize(function(){
     if(window.innerWidth < 600){
       
-      $('.about-the-project').addClass('center')
-      $('.instruments-and-genre').addClass('center')
+      $('.about-the-project').addClass('center');
+      $('.instruments-and-genre').addClass('center');
     }else{
      
-      $('.about-the-project').removeClass('center')
-      $('.instruments-and-genre').removeClass('center')
+      $('.about-the-project').removeClass('center');
+      $('.instruments-and-genre').removeClass('center');
     }
     
   });
